36446fdce47a8cc979c65f4ec7dd2a276899fc39
SkylordJoel <joelsminecraft@phillipswa.com>
2014-05-25 09:01:57 +0800

    Updated APIs
    Updated UE api, added Resonant-Engine/Calclavia Core. Generators, here
    I come!

---
+++ forge/mcp/src/minecraft/calclavia/api/mffs/fortron/IFortronFrequency.java
@@ -0,0 +1,8 @@
+package calclavia.api.mffs.fortron;
+
+import resonant.api.blocks.IBlockFrequency;
+
+public interface IFortronFrequency extends IFortronStorage, IBlockFrequency
+{
+
+}
---
+++ forge/mcp/src/minecraft/calclavia/api/mffs/fortron/IServerThread.java
@@ -0,0 +1,9 @@
+package calclavia.api.mffs.fortron;
+
+/** Implemented by threads that exist server side.
+ * 
+ * @author Calclavia */
+public interface IServerThread
+{
+
+}
